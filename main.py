7f653d0de77749f0c4d6ec33e6705f64b7c52f99
lasselian <lassesorensen80@gmail.com>
2026-03-13 08:50:59 +0100

    1.4.1
    silly bug: fixed bug where new version would get prompted because i forgot to update version in main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtGui import QDesktopServices
 from PyQt6.QtCore import QUrl
 
-VERSION = "1.4"
+VERSION = "1.4.1"
 
 def _create_task_safe(coro):
     """Schedule an async task safely from synchronous Qt context.
