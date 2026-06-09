8fbad90d122aa3107de736af10bf73b79bdf2922
Niklas Thorild <niklas@thorild.se>
2026-06-09 16:28:29 +0200

    flake: update to 1.5.4

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     {
       packages = forAllSystems (pkgs:
         let
-          version = "1.5.3";
+          version = "1.5.4";
 
           pythonEnv = pkgs.python3.withPackages (ps: with ps; [
             pyqt6
@@ -42,7 +42,7 @@
               owner = "lasselian";
               repo = "prism-desktop";
               rev = version;
-              hash = "sha256-2xKMSXte8mAMme0CxUw8dhUwc86ig0JBhvp3LoDtVA4=";
+              hash = "sha256-SSiaU8hvJrc4nol6oHyuu2OKsKkK72LNrolFh/JTfP4=";
             };
 
             nativeBuildInputs = [
